838af807bb40ca86f846a468a53d09c5ab43cfe1
jtavis <jennifertavis@gmail.com>
2016-04-05 20:45:08 -0400

    April 5 commit 1

---
+++ class-work/jquery-review/js/app.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+;
+
+	$('#boxOne').click(function() {
+		$('#boxOne').css('width','500px').css('height','500px');
+		$('#boxOne').css('background-color','red').text('New Text1');
+	})
+
+	$('#boxTwo').click(function() {
+		$('#boxTwo').css('width','500px').css('height','500px');
+		$('#boxTwo').css('background-color','red').text('New Text2');
+	})
+
+})
+
+
