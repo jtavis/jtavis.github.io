198cb0679b6891421c2617a9800da8557ac77eae
jtavis <jennifertavis@gmail.com>
2016-04-11 20:34:20 -0400

    April 11 commit 3

--- homework/temp-converter/js/app.js
+++ homework/temp-converter/js/app.js
@@ -1,21 +1,32 @@
 $(document).ready(function () {
-	$('#degF').keyup(presto);
-	$('#degC').keyup(presto);
+	$('#degF').keyup(convertF);
+	$('#degC').keyup(convertC);
 
-	function presto () {
-	var degF = parseFloat($('#degF').val());
-	var degC = parseFloat($('#degC').val());
-
-
-		if (degF!=null) {
-			$('#output').text((degF* 1.8) + 32);
+	function convertF () {
+	var degF = parseInt($('#degF').val());
+	$('#output').html(Math.round((degF-32)/1.8)+"&deg;C");
+		if (degF<32) {
+			$('#output2').text("Wear a coat!");
 		}
-		else if (degC!=null){
-			$('#output').text((degC - 32) / 1.8);
+		else if (degF>=32 && degF<70) {
+			$('#output2').text("Wear a sweater!");
 		}
-		else{
-			alert('This is not a number');
+		else {
+			$('#output2').text("Wear shorts!");
 		}
+	}
 
+	function convertC () {
+	var degC = parseInt($('#degC').val());
+	$('#output').html(Math.round((degC* 1.8) + 32)+"&deg;F");
+		if (degF<0) {
+			$('#output2').text("Wear a coat!");
+		}
+		else if (degF>=0 && degF<21) {
+			$('#output2').text("Wear a sweater!");
+		}
+		else {
+			$('#output2').text("Wear shorts!");
+		}
 	}
 });
