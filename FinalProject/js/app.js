41290c0825a13e6b860f5e9610d02a7d5a3f7d2f
jtavis <jennifertavis@gmail.com>
2016-04-23 22:14:26 -0400

    April 23 commit 1

--- FinalProject/js/app.js
+++ FinalProject/js/app.js
@@ -1,40 +1,46 @@
 $(document).ready(initMap);
 
 
-	function initMap() {
-		var mapDiv = document.getElementById('map');
-		var map = new google.maps.Map(mapDiv, {
-		  center: {lat: 40.7484444, lng: -73.9878441},
-		  zoom: 10
+function initMap() {
+	var mapDiv = document.getElementById('map');
+	var map = new google.maps.Map(mapDiv, {
+	  center: {lat: 40.7484444, lng: -73.9878441},
+	  zoom: 10
 	});
+};
 
-	var geocoder;
-  	var map;
-  	function initialize() {
-	    geocoder = new google.maps.Geocoder();
-	    var latlng = new google.maps.LatLng(-34.397, 150.644);
-	    var mapOptions = {
-	    zoom: 8,
-	    center: latlng
-    }
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  }
+// 	var geocoder;
+//   	var map;
+//   	function initialize() {
+// 	    geocoder = new google.maps.Geocoder();
+// 	    var latlng = new google.maps.LatLng(-34.397, 150.644);
+// 	    var mapOptions = {
+// 	    zoom: 8,
+// 	    center: latlng
+//     }
+//     map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//   }
 
-  function codeAddress() {
-    var address = document.getElementById("address").value;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map,
-            position: results[0].geometry.location
-        });
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
-  }
+function codeAddress() {
+	var address = document.getElementById("address").value;
+	geocoder.geocode( { 'address': address}, function(results, status) {
+	  if (status == google.maps.GeocoderStatus.OK) {
+	    map.setCenter(results[0].geometry.location);
+	    var marker = new google.maps.Marker({
+	        map: map,
+	        position: results[0].geometry.location
+	    });
+	  } else {
+	    alert("Geocode was not successful for the following reason: " + status);
+	  }
+	});
 }
+
+$('#searchButton').click(function () {
+	$('#enterSrch').slideToggle();
+	$('#posts').slideToggle();
+	$('#sidebar').slideToggle();
+})
 
 
 
